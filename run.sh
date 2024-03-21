6789164038c9779aebafd6fbc13bfe009e7134ee
John Alaydrus <142376121+johnlucky88@users.noreply.github.com>
2024-03-22 04:46:26 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-76b24758-5e92-48c8-a994-0651084b65a0/)
 
 
 echo "$curl_output" >> Update.tsx
